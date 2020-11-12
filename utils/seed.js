ded374f16dd75fcc41922d41ff6858a08cae3c97
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-11-11 21:41:00 -0600

    updating main handlebars

--- utils/seed.js
+++ utils/seed.js
@@ -1,7 +1,7 @@
 module.exports = function(table) {
    if (process.env !== "production") {
       console.log("\n🚀 init table seed");
-      return table.create({ comment: "🚀 init" });
+      return table.create({ group_name : "🚀 init" });
    }
    return Promise.resolve();
 };
