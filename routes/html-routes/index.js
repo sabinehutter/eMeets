cd4b301299aec62501386d2580adf1eed5685b0a
allie Lewis <allielewis07@gmail.com>
2020-11-14 18:32:47 -0500

    modfied newMember.js

--- routes/html-routes/index.js
+++ routes/html-routes/index.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const commentsRoute = require("./comments");
 const memberRoute = require("./member");
 const groupRoute = require("./group");
+const newMemberRoute = require("./newMember");
 
 
 router.use("/member", memberRoute);
 
 router.use("/group", groupRoute);
+
+router.get("/newMember", newMemberRoute);
 // get route -> index
 
 router.get("/", (req, res) => {
