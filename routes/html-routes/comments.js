344bbffeefe459bc7ed54d71578db22078ed819b
allie Lewis <allielewis07@gmail.com>
2020-11-13 20:54:51 -0500

    modifed and added files

--- routes/html-routes/comments.js
+++ routes/html-routes/comments.js
@@ -1,13 +1,17 @@
+
 const express = require("express");
 const router = express.Router();
 const db = require("../../models");
 
-// routing (html) /commnets
-router.get("/", (req, res) => {
+// routing (html) /comments
+router.get("/",(req, res,next) => {
    // get comments from db and send to template
-   db.Test.findAll({})
-      .then(comments => res.render("index", { comments: comments }))
-      .catch(err => {
+   db.Group.findAll({})
+      .then (result => {
+         console.log(result);
+         res.json(result);
+      }
+      ) .catch(err => {
          res.status(500);
          next(err);
       });
