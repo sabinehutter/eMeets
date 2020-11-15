c95204a35ce249511a22ed935ad0b01a0134e923
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-11-14 23:44:05 -0500

    updating submit button post requests

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const commentsRoute = require("./comments");
 const groupRoute = require("./group");
+const newMember = require("./newMember");
 const router = express.Router();
 
 router.use("/comments", commentsRoute);
 
 router.use("/group", groupRoute);
+
+router.use("/newMember", newMember);
 
 module.exports = router;
