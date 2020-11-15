06fcaeeb3013a573184dfd2dec51114ee757535e
Kuanting Pan <pankuanting@gmail.com>
2020-11-14 22:14:06 -0500

    merigng latest

--- routes/html-routes/index.js
+++ routes/html-routes/index.js
@@ -4,11 +4,14 @@ const commentsRoute = require("./comments");
 const memberRoute = require("./member");
 const groupRoute = require("./group");
 const newMemberRoute = require("./newMember");
+const createGroupRoute = require("./createGroup");
 
 
 router.use("/member", memberRoute);
 
 router.use("/group", groupRoute);
+
+router.get("/createGroup", createGroupRoute);
 
 router.get("/newMember", newMemberRoute);
 // get route -> index
