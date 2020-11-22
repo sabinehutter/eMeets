452c04ccc86049c2e4a281e3e9a2a5336dfc2370
Kuanting <kuanting@kuan-tings-imac.myfiosgateway.com>
2020-11-22 00:16:11 -0500

    fix travis

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,8 +1,8 @@
-module.exports = function(err, req, res) {
+module.exports = function(err, res) {
    // potential switch based on error code or msg
 
    // log error to monitor
    // for development
    console.error(err.stack);
-   // return res.redirect("/");
+   return res.redirect("/");
 };
